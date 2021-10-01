8b9d648b8f7d5a2841366dbe8002927e2f818e7e
EstefanyCB <89163045+EstefanyCB@users.noreply.github.com>
2021-09-30 22:46:29 -0600

    Valores para la pantalla

--- D2_Laboratorio5_UART/src/main.cpp
+++ D2_Laboratorio5_UART/src/main.cpp
@@ -17,7 +17,7 @@
 #define Poten1 35
 #define Poten2 34
 
-#define LedR 18  
+#define LedR 18
 #define LedG 17
 #define LedB 16
 
@@ -31,7 +31,8 @@
 //Prototipos de funciones
 //******************************************************************************************
 void Potenciometros(void);
-
+void PantallLCD(void);
+void ValoresPotenciometros(void);
 //******************************************************************************************
 //Variables globales
 //******************************************************************************************
@@ -44,13 +45,18 @@ double VoltajePoten2 = 0;
 
 double alpha = 0.09; // Filtro para estabilizar
 
+LiquidCrystal LCD(RS, E, D4, D5, D6, D7);
+uint8_t P1Centena, P1Decena, P1Unidad;
+uint8_t P2Centena, P3Decena, P4Unidad;
 //******************************************************************************************
 //Configuraciones
 //******************************************************************************************
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Potenciometros();
+
+  LCD.begin(16, 2);
 }
 
 //******************************************************************************************
@@ -59,6 +65,8 @@ void setup()
 void loop()
 {
   Potenciometros();
+  ValoresPotenciometros();
+  delay(500);
 }
 
 //******************************************************************************************
@@ -66,15 +74,29 @@ void loop()
 //******************************************************************************************
 void Potenciometros(void)
 {
-  ADCPoten1 = (analogReadMilliVolts(Poten1)*255) / 3150;
+  ADCPoten1 = (analogReadMilliVolts(Poten1) * 255) / 3150;
   VoltajePoten1 = (alpha * ADCPoten1) + ((1.0 - alpha) * VoltajePoten1); //Filtro pasa bajas
   Serial.print("Voltaje potenciometro 1: ");
   Serial.println(VoltajePoten1);
 
-  ADCPoten2 = (analogReadMilliVolts(Poten2)*255) / 3150;
+  ADCPoten2 = (analogReadMilliVolts(Poten2) * 255) / 3150;
   VoltajePoten2 = (alpha * ADCPoten2) + ((1.0 - alpha) * VoltajePoten2); //Filtro pasa bajas
   Serial.print("Voltaje potenciometro 2: ");
   Serial.println(VoltajePoten2);
 }
 
-//
+//******************************************************************************************
+//Valores Potenciometros
+//******************************************************************************************
+void ValoresPotenciometros(void)
+{
+  P1Centena = VoltajePoten1 / 100;
+  P1Decena = (VoltajePoten1 - (P1Centena * 100)) / 10;
+  P1Unidad = (VoltajePoten1 - P1Centena * 100) - (P1Decena * 10);
+  Serial.print("P1 Centena");
+  Serial.println(P1Centena);
+  Serial.print("P1 Decena");
+  Serial.println(P1Decena);
+  Serial.print("P1 Unidad");
+  Serial.println(P1Unidad);
+}
